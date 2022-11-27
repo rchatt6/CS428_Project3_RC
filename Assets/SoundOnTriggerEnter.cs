cd63d464e51d082e0e29e5a9df51fd9e8e450303
benhern <43895569+benhern@users.noreply.github.com>
2022-11-26 22:19:39 -0600

    fixed lights and tooth

--- Assets/SoundOnTriggerEnter.cs
+++ Assets/SoundOnTriggerEnter.cs
@@ -6,11 +6,18 @@ public class SoundOnTriggerEnter : MonoBehaviour
 {
     public AudioClip[] audio = new AudioClip[4];
     public GameObject[] lights = new GameObject[4];
+    int count = 0;
 
 
     void OnTriggerEnter()
     {
         int sound = Random.Range(0, audio.Length);
+        lights[0].SetActive(false);
+        lights[1].SetActive(false);
+        lights[2].SetActive(false);
+        lights[3].SetActive(false);
+
+        
 
         //AudioSource.PlayClipAtPoint(audio[sound], transform.position);
         if(!GetComponent<AudioSource>().isPlaying)
