b7a55328e44df7cb2bafd545231779253074e097
benhern <43895569+benhern@users.noreply.github.com>
2022-11-26 18:35:15 -0600

    Dial and lights

--- Assets/SoundOnTriggerEnter.cs
+++ Assets/SoundOnTriggerEnter.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class SoundOnTriggerEnter : MonoBehaviour
 {
     public AudioClip[] audio = new AudioClip[4];
-    // Start is called before the first frame update
-    /*void Start()
-    {
-        
-    }
+    public GameObject[] lights = new GameObject[4];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }*/
 
     void OnTriggerEnter()
     {
         int sound = Random.Range(0, audio.Length);
+
+
+        for(int i = 0; i < 4;i++ ){
+            Debug.Log(lights[i].activeInHierarchy);
+                    Debug.Log(lights[0].name);
+
+
+            if(lights[i].activeInHierarchy){
+                lights[i].SetActive(false);
+            }
+        }
         //AudioSource.PlayClipAtPoint(audio[sound], transform.position);
         if(!GetComponent<AudioSource>().isPlaying)
         {
             GetComponent<AudioSource>().PlayOneShot(audio[sound]);
+            lights[sound].SetActive(true);
         }
         
     }
